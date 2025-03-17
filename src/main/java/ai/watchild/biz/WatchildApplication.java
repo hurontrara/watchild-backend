67ed7dc24c70fb06abba761411c3a45d068f84c3
sunbin <csb7386@gmail.com>
2025-03-17 09:59:18 +0900

    initialize project with Gradle setup and Spring Boot configuration

---
+++ src/main/java/ai/watchild/biz/WatchildApplication.java
@@ -0,0 +1,13 @@
+package ai.watchild.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WatchildApplication.class, args);
+    }
+
+}
